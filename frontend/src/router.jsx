8bac4e518f4204a94a6bf2b2f7d672821d11af1a
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-12-04 15:30:00 +0100

    restructure app layout with Navbar and Footer components, add routing, and update styles

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,10 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Homepage from "./routes/Homepage/Homepage";
+import Signup from "./routes/Signup/Signup";
+import Landingpage from "./routes/Landingpage/Landingpage";
+
 export const router = createBrowserRouter([
   {
     element: <App />,
+    path: "/",
     children: [
       {
-        Path: "/",
-        element: <HomePage />,
+        index: true,
+        element: <Landingpage />,
+      },
+      {
+        path: "homepage",
+        element: <Homepage />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
       },
     ],
   },
